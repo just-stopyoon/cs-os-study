0b4e1611967e0a4f87b19607cb2bb6e00792cc12
SNXWXH <snxwxh@gmail.com>
2025-02-28 17:15:41 +0900

    next.config.js 코드 수정

--- next.config.js
+++ next.config.js
@@ -3,9 +3,11 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.jsx',
 });
 
-module.exports = withNextra({
+const nextConfig = {
   output: 'export',
-});
+};
+
+module.exports = withNextra(nextConfig);
 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
